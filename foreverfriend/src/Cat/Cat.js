6908be9795d1042f4673800e00353ed2810f883a
Priscilla Martinez <priscillamartinez@PriscillasMBP2.attlocal.net>
2022-02-16 16:11:54 -0800

    Getting individual data for selected cat

--- foreverfriend/src/Cat/Cat.js
+++ foreverfriend/src/Cat/Cat.js
@@ -2,8 +2,24 @@ import { useParams } from 'react-router'
 import CatData from '../CatData'
 
 function Cat(){
+    let catName = useParams().catName
+
+    console.log("useParam Cat Name: ",catName)
+
+    const paramCatData = CatData.filter(CatData => CatData.name === catName).map(data => {
+        return(
+            <>
+                <h1>{data.name}</h1>
+                <h2>{data.breeds.primary}</h2>
+            </>
+        );
+    })
+
     return(
-        <h1>Individual Cat Page</h1>
+        <>
+            <h1>Individual Cat Page</h1>
+            {paramCatData}
+        </>
     )
 }
 
